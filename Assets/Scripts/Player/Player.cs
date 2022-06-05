df99c9a055124cf346468ef991fa841d3f234e11
TonySwity <goldocean@live.com>
2022-06-05 20:56:27 +0300

    add mousePosition in Plain

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,18 +5,31 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
 
+    private Camera _viewCamera;
     private PlayerController _controller;
 
     private void Start()
     {
         _controller = GetComponent<PlayerController>();
+        _viewCamera = Camera.main;
     }
 
     private void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * _moveSpeed;
         _controller.Move(moveVelocity);
+
+        Ray ray = _viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance));
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            _controller.LookAt(point);
+        }
     }
 }
 
