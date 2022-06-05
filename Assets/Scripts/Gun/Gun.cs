5544b9999a4adc364637b49630603ab64796c192
TonySwity <goldocean@live.com>
2022-06-05 22:48:19 +0300

    add method shoot and add Gunprefab -> projectile spawn

--- Assets/Scripts/Gun/Gun.cs
+++ Assets/Scripts/Gun/Gun.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _muzzle;
+    [SerializeField] private Projectile _projectile;
+    [SerializeField] private float _msBetweenShoot = 100f;
+    [SerializeField] private float _muzzleVelocity = 35f;
 
-    // Update is called once per frame
-    void Update()
+    private float _nextShootTime;
+    private float _ms = 1000f;
+    public void Shoot()
     {
+        if (Time.time > _nextShootTime)
+        {
+            _nextShootTime = Time.time + _msBetweenShoot / _ms;
+        }
         
+        Projectile projectile = Instantiate(_projectile, _muzzle.position, _muzzle.rotation);
+        projectile.SetSpeed(_muzzleVelocity);
     }
 }
