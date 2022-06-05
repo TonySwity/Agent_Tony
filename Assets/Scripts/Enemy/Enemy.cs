acb63254f1cf7ca1f306a8392f7bc9c3ece79fa3
TonySwity <goldocean@live.com>
2022-06-06 00:17:03 +0300

    set color

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -5,14 +6,24 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent _pathfinder;
+    private Transform _target;
 
     private void Start()
     {
         _pathfinder = GetComponent<NavMeshAgent>();
+        _target = GameObject.FindObjectOfType<Player>().transform;
+        StartCoroutine(UpdatePath());
     }
-
-    private void Update()
+    
+    private IEnumerator UpdatePath()
     {
-        
+        float refreshRate = .25f;
+
+        while (_target != null)
+        {
+            Vector3 targetPosition = new Vector3(_target.position.x, 0f, _target.position.z);
+            _pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
     }
 }
