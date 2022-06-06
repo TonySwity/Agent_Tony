d0c22cbc028dc662c0cd0c0573702d3f5ac557de
TonySwity <goldocean@live.com>
2022-06-06 23:04:23 +0300

    impliments IDamageable

--- Assets/Scripts/Gun/Projectile/Projectile.cs
+++ Assets/Scripts/Gun/Projectile/Projectile.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private LayerMask _collisionMask;
+    
     private float _speed = 10f;
+    private float _damage = 1f;
 
     public void SetSpeed(float newSpeed)
     {
@@ -11,6 +14,31 @@ public class Projectile : MonoBehaviour
 
     private void Update()
     {
+        float moveDistance = _speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+
+    private void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance,_collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    private void OnHitObject(RaycastHit hit)
+    {
+        IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
+
+        if (damageableObject != null)
+        {
+            damageableObject.TakeHit(_damage, hit);
+        }
+        
+        Destroy(gameObject);
     }
 }
