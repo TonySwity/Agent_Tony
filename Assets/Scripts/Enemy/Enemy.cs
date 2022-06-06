fc69f9af96839e0b30d68f3493f1fd985bce2822
TonySwity <goldocean@live.com>
2022-06-06 23:02:58 +0300

    Enemy inherited LivingEntity

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class Enemy : MonoBehaviour
+public class Enemy : LivingEntity
 {
     private NavMeshAgent _pathfinder;
     private Transform _target;
