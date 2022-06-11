c1819a92b07fc80538891c6bf0641e42adc25aae
TonySwity <goldocean@live.com>
2022-06-11 15:24:07 +0300

    add NextWave, OnEnemyDied

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private Wave[] _waves;
+
+    private Wave _currentWave;
+    private int _currentWaveNumber;
+    private int _enemiesRemainingToSpawn;
+    private int _enemiesRemainingAlive;
+    private float _nextSpawnTime;
+
+    
+    private void Start()
     {
-        
+        NextWave();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_enemiesRemainingToSpawn > 0 && Time.time > _nextSpawnTime)
+        {
+            _enemiesRemainingToSpawn--;
+            _nextSpawnTime = Time.time + _currentWave.TimeBetweenSpawns;
+
+            Enemy spawnedEnemy = Instantiate(_enemy, Vector3.zero, Quaternion.identity);
+            spawnedEnemy.OnDeath += OnEnemyDeath;
+        }
     }
+    
+    private void NextWave()
+    {
+        _currentWaveNumber++;
+
+        if (_currentWaveNumber - 1 < _waves.Length)
+        {
+            _currentWave = _waves[_currentWaveNumber - 1];
+            
+            _enemiesRemainingToSpawn = _currentWave.EnemyCount;
+            _enemiesRemainingAlive = _enemiesRemainingToSpawn;
+        }
+
+    }
+
+    private void OnEnemyDeath()
+    {
+        _enemiesRemainingAlive--;
+
+        if (_enemiesRemainingAlive == 0)
+        {
+            NextWave();
+        }
+    }
+    
+    [System.Serializable]
+    public class Wave
+    {
+        public int EnemyCount;
+        public float TimeBetweenSpawns;
+    }
+
 }
